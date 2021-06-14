1451b4b8f756db23506fde4723254744200237c6
elibahlcke <75950359+elibahlcke@users.noreply.github.com>
2021-06-14 01:06:02 -0300

    Added complete project with babel transcriptions

---
+++ js/index.bundle.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var color = document.querySelector("#color");
+var salida = document.querySelector("#salida");
+color.addEventListener("input", function () {
+  var value = color.value;
+  salida.innerHTML = value;
+  salida.style.background = value;
+  salida.style.color = "#fff";
+  salida.style.borderColor = value;
+
+});
